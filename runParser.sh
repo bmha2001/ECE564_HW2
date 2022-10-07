79cdad9a774a8fba0e0575fc1b060fd3ad7a006e
Brian Mhatre <bmha2001@yahoo.com>
2022-10-06 21:21:24 -0500

    bug fixes

--- runParser.sh
+++ runParser.sh
@@ -1,3 +1,6 @@
 rm -rf *.dat
-python parser.py ebay_data/items-*.json
+python parser.py ebay_data/items-0.json
 sort users.dat | uniq -u > users_unique.dat
+sort bids.dat | uniq -u > bids_unique.dat
+sort categories.dat | uniq -u > categories_unique.dat
+sort items.dat | uniq -u > items_unique.dat
