5e079d84b30edd2867ef5cc0ce3d0687a01ae3ca
jmondi744 <102432792+jmondi744@users.noreply.github.com>
2022-10-07 17:15:02 -0500

    adjusted select

--- query5.sql
+++ query5.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(DISTINCT UserID) FROM Items, EbayUser WHERE Items.SellerID = EbayUser.UserID AND EbayUser.Rating > 1000;
+SELECT COUNT(DISTINCT EbayUser.UserID) FROM Items, EbayUser WHERE Items.SellerID = EbayUser.UserID AND EbayUser.Rating > 1000;
